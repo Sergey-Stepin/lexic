3248a57dc185dc58d6389d0dea16eef8eeadbdaa
stepin <stepin.developer@gmail.com>
2024-01-30 00:25:48 +0100

    added AppShell

---
+++ src/main/java/services/stepin/home/lexic/AppShell.java
@@ -0,0 +1,8 @@
+package services.stepin.home.lexic;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+@PWA(name = "Lexic app", shortName = "lexic")
+public class AppShell implements AppShellConfigurator {
+}
